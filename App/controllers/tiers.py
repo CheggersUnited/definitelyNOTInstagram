0b3800482bee71e1ca2e52f765b1cc074d4a1032
Trey Murray <77852326+TreyMurray00@users.noreply.github.com>
2022-09-26 18:33:04 -0400

    Update tiers.py

--- App/controllers/tiers.py
+++ App/controllers/tiers.py
@@ -1,31 +1,43 @@
 from models import User
 from controllers import user
-import random
-from enum import Enum
-
-class Tiers(Enum):
-    "Tier1": 1
-    "Tier2": 1
-    "Tier3": 1
-    "Tier4": 1
-    "Tier5": 1
-    "Tier6": 1
-    "Tier7": 1
-    "Tier8": 1
-
-
 
 def like(username):
-    pass
+    user = user.get_user(username=username)
+    if user.tier == 1:
+        user.points += user.tier
+    else:
+        user.points += (1 - ((user.tier - 1)/10))
+    return True
+
 
 def dislike(username):
-    pass
+    user = user.get_user(username=username)
+    if (user.points - 0.5) >= 0:
+        user.points -= 0.5
+    return True
+    
 
-def tier_upgrade(username):
-    pass
+def tier_update(username):
+    user = user.get_user(username=username)
+    if(user.points >= 10 and user.points <= 20):
+        user.tier = 2
+    elif(user.points >= 21 and user.points <= 30):
+        user.tier = 3
+    elif(user.points >= 31 and user.points <= 40):
+        user.tier = 4
+    elif(user.points >= 41 and user.points <= 50 ):
+        user.tier = 5
+    elif(user.points >= 51 and user.points <= 60):
+        user.tier = 6
+    elif(user.points >= 61 and user.points <= 70):
+        user.tier = 7
+    elif(user.points >= 71 and user.points <= 80):
+        user.tier = 8
+    elif(user.points >= 81 and user.points <= 90):
+        user.tier = 9
+    elif(user.points >= 91 and user.points <= 100):
+        user.tier = 10
+    return True
 
-def tier_downgrade(username):
-    pass
-
-def point_calculation(username):
-    pass
+    def limits(username):
+        pass
