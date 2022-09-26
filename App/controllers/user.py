b7af3b3b5808e04128fe8c1745a39aafd307d350
ShivsTKD <shivanmaharaj.sm@gmail.com>
2022-09-26 22:56:46 +0000

    Tried resolving issues

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,20 +1,10 @@
 from App.models import User
 from App.database import db
 from sqlalchemy.exc import IntegrityError
-from App.controllers import firebaseconfig
-from os import remove
 
 def get_all_users():
     users = Profile.query.all()
     return users
-
-# def get_programmes():
-#     programmeList = []
-#     programmes = Programme.query.all()
-#     for p in programmes:
-#         if p.name not in programmeList:
-#             programmeList.append(p.name)
-#     return programmeList
 
 def get_user(username):
     return User.query.filter_by(username=username).first()
