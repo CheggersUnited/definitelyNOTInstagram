b9c705a4666e7405698455e6233700d023b1fa69
Trey Murray <77852326+TreyMurray00@users.noreply.github.com>
2022-10-03 19:13:53 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-SQLALCHEMY_DATABASE_URI = "postgres://kdawolxegettjr:9b044aa8bfc4ccb9360ceeb2c7373dbcfe9ec8eb57c94e14345d43b084e88794@ec2-34-200-205-45.compute-1.amazonaws.com:5432/dbig21ehld4gps"
+SQLALCHEMY_DATABASE_URI = "postgresql://kdawolxegettjr:9b044aa8bfc4ccb9360ceeb2c7373dbcfe9ec8eb57c94e14345d43b084e88794@ec2-34-200-205-45.compute-1.amazonaws.com:5432/dbig21ehld4gps"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = timedelta(days=7)
 ENV = "DEVELOPMENT"
