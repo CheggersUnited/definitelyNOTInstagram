916bb039447ac1e8e5d84260c641c160f18f3879
ShivsTKD <shivanmaharaj.sm@gmail.com>
2022-10-04 20:53:41 +0000

    More work to controllers

--- App/controllers/tiers.py
+++ App/controllers/tiers.py
@@ -1,25 +1,26 @@
 from App.models import User
 from App.database import db
+from App.controllers.user import get_user
 
-def interact(username):
-    user = User.query.filter_by(username=username).first()
+def interact(id):
+    user = get_user(id)
     if user.tier == 1:
         user.points += user.tier
     else:
         user.points += (1 - ((user.tier - 1)/10))
-    tier_update(username)
+    tier_update(id)
     db.session.commit()
     return True
 
-def like_a_pic(username):
-    interact(username)
+def like_a_pic(id):
+    interact(id)
     return True
 
-def dislike_a_pic(username):
-    user = User.query.filter_by(username=username).first()
+def dislike_a_pic(id):
+    user = get_user(id)
     if (user.points - 0.5) >= 0:
         user.points -= 0.5
-        tier_update(username)
+        tier_update(id)
         db.session.commit()
     return True
 
@@ -34,8 +35,8 @@ def update_limit(user):
     db.session.commit()   
     return True
 
-def tier_update(username):
-    user = User.query.filter_by(username=username).first()
+def tier_update(id):
+    user = get_user(id)
     if (user.points % 10) != 0:
         user.tier = int(user.points / 10) + 1
     else:
