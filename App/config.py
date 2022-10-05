46e7345cbe496f990b74c9b5991acc47bfab85d1
Trey Murray <77852326+TreyMurray00@users.noreply.github.com>
2022-10-05 19:38:15 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 
-# SQLALCHEMY_DATABASE_URI = "postgresql://asxkpiraqlaoqt:b5c3a1e67ee1f07a8c7609d2b2c32fa9ea7f8740bd74ccb57cc0bc081ba7b7ed@ec2-54-227-248-71.compute-1.amazonaws.com:5432/d6r48ejm7f1fsr"
-SQLALCHEMY_DATABASE_URI = "sqlite://///workspace/definitelyNOTInstagram/App/test.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://zwqyodsovxkzbg:fac4cc02ab1f949085f1b75f2cf313fbddfc92a8858bb8668474152f86b03583@ec2-35-168-122-84.compute-1.amazonaws.com:5432/de7mgrp7ccussv"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = timedelta(minutes=30)
 ENV = "DEVELOPMENT"
