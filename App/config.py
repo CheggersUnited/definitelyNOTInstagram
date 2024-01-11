1509c4c0eccfb768dbf09a8da68320f6a7c1841c
ShivsTKD <shivanmaharaj.sm@gmail.com>
2024-01-11 21:57:54 +0000

    Updated some files

--- App/config.py
+++ App/config.py
@@ -1,6 +1,6 @@
 # SQLALCHEMY_DATABASE_URI = "postgresql://asxkpiraqlaoqt:b5c3a1e67ee1f07a8c7609d2b2c32fa9ea7f8740bd74ccb57cc0bc081ba7b7ed@ec2-54-227-248-71.compute-1.amazonaws.com:5432/d6r48ejm7f1fsr"
-# SQLALCHEMY_DATABASE_URI = "sqlite://///workspace/definitelyNOTInstagram/App/test.db"
-SQLALCHEMY_DATABASE_URI = "postgresql://cfsihsmmpyoqpt:4546a443cb7febb3a7c10a2c45fc2bea73287c584e7afeb59e14481b560d7a40@ec2-3-223-242-224.compute-1.amazonaws.com:5432/d15605s3tie8vh"
+SQLALCHEMY_DATABASE_URI = "sqlite://///workspace/definitelyNOTInstagram/App/test.db"
+# SQLALCHEMY_DATABASE_URI = "postgresql://cfsihsmmpyoqpt:4546a443cb7febb3a7c10a2c45fc2bea73287c584e7afeb59e14481b560d7a40@ec2-3-223-242-224.compute-1.amazonaws.com:5432/d15605s3tie8vh"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "PRODUCTION"
